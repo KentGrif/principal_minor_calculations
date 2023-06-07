0e2cfd5262e1c9a29d443ac0b3039fc40b43d74c
Kent Griffin <bluegriffin@hotmail.com>
2023-06-07 15:12:52 -0700

    Added structure to hold PM calculation info and warnings.  Formatting.  Added tests.

--- Python/principal_minor_calculations.py
+++ Python/principal_minor_calculations.py
@@ -2,34 +2,46 @@ import numpy as np
 
 
 # Default data type for computation
-DTYPE = np.cdouble     # complex number, represented by two double-precision floats
+DTYPE = np.cdouble  # complex number, represented by two double-precision floats
+
+
+class PMInfo:
+    """
+    Stores information and warnings about the principal minor computations.
+    """
+    def __init__(self):
+        self.smallest_pivot = np.inf
+        self.number_of_times_ppivoted = 0
+        self.warn_not_odf = False
+        self.warn_under_determined = False
+        self.warn_inconsistent = False
 
 
 def mat2pm(a: np.array, thresh=1e-5):
     """
-    mat2pm returns a 2^n - 1 vector of all the principal minors of the matrix a.
+    mat2pm returns a 2^n - 1 vector of all the principal minors of the matrix "a".
     :param a: n x n input matrix
     :param thresh: Threshold for psuedo-pivoting.  Pseudo-pivoting will occur when a pivot smaller in magnitude than
         thresh arises.  Set thresh = 0 to never pseudo-pivot except for a pivot of exactly zero.
-    :return: Tuple of (array of principal minors: np.array, computation info: str).
+    :return: Tuple of (array of principal minors: np.array, computation info: PMInfo).
         The structure of pm, where |a[v]| is the principal minor of "a" indexed by the vector v is:
         pm: |a[1]|, |a[2]|, |a[1 2]|, |a[3]|, |a[1 3]|, |a[2 3]|, |a[1 2 3]|,...
     """
-    assert(len(a.shape) == 2 and a.shape[0] == a.shape[1])
+    assert (len(a.shape) == 2 and a.shape[0] == a.shape[1])
     a = a.astype(DTYPE)
     n = a.shape[0]
-    scale = np.sum(abs(a))/(n*n)    # average magnitude of matrix
+    scale = np.sum(abs(a)) / (n * n)  # average magnitude of matrix
     if scale == 0:
-        scale = 1               # prevent divide by 0 if matrix is zero
-    ppivot = scale              # value to use as a pivot if near 0 pivot arises
+        scale = 1  # prevent divide by 0 if matrix is zero
+    ppivot = scale  # value to use as a pivot if near 0 pivot arises
 
     zeropivs = []
-    pm = np.zeros(2**n - 1, dtype=DTYPE)     # where the principal minors are stored
-    ipm = 0                     # index for storing principal minors
+    pm = np.zeros(2 ** n - 1, dtype=DTYPE)  # where the principal minors are stored
+    ipm = 0  # index for storing principal minors
 
     # q is the input queue of unprocessed matrices
-    q = [a]                     # initial queue just has 1 matrix to process
-    pivmin = np.inf             # keep track of smallest pivot actually used
+    q = [a]  # initial queue just has 1 matrix to process
+    info = PMInfo()
 
     #
     # Main 'level' loop
@@ -39,8 +51,8 @@ def mat2pm(a: np.array, thresh=1e-5):
         n1 = q[0].shape[0]
         # The output queue has twice the number of matrices, each one smaller
         # in row and col dimension
-        qq = [None] * 2*nq      # zeros(n1-1, n1-1, nq*2);
-        ipm1 = 0                # for indexing previous pm elements
+        qq = [None] * 2 * nq  # zeros(n1-1, n1-1, nq*2);
+        ipm1 = 0  # for indexing previous pm elements
         for i in range(nq):
             a = q[i]
             pm[ipm] = a[0, 0]
@@ -51,8 +63,8 @@ def mat2pm(a: np.array, thresh=1e-5):
                     # Pivot nearly zero, use "pseudo-pivot"
                     pm[ipm] += ppivot
                     abspiv = np.abs(pm[ipm])
-                if abspiv < pivmin:
-                    pivmin = abspiv
+                if abspiv < info.smallest_pivot:
+                    info.smallest_pivot = abspiv
                 b = a[1:, 1:]
                 d = a[1:, 0] / pm[ipm]
                 c = b - np.outer(d, a[0, 1:])
@@ -60,12 +72,12 @@ def mat2pm(a: np.array, thresh=1e-5):
                 # Order the output queue to make the elements of pm come out in
                 # the correct order.
                 qq[i] = b
-                qq[i+nq] = c
+                qq[i + nq] = c
             if i > 0:
                 # if not the first iteration, to convert from a general pivot to a principal
                 # minor, we need to multiply by every element of the pm matrix
                 # we have already generated, in the order that we generated it.
-                pm[ipm] = pm[ipm]*pm[ipm1]
+                pm[ipm] = pm[ipm] * pm[ipm1]
                 ipm1 += 1
             ipm += 1
         q = qq
@@ -78,17 +90,34 @@ def mat2pm(a: np.array, thresh=1e-5):
     for i in reversed(zeropivs):
         mask = i + 1
         delta = _msb(mask)
-        delta2 = 2*delta
+        delta2 = 2 * delta
         ipm = (mask & ~delta)
         if ipm == 0:
             pm[i] -= ppivot
         else:
             ipm -= 1
-            pm[i] = (pm[i]/pm[ipm] - ppivot)*pm[ipm]
-        for j in range(mask+delta2-1, len(pm), delta2):
-            pm[j] -= ppivot*pm[j-delta]
+            pm[i] = (pm[i] / pm[ipm] - ppivot) * pm[ipm]
+        for j in range(mask + delta2 - 1, len(pm), delta2):
+            pm[j] -= ppivot * pm[j - delta]
 
-    return pm, f'mat2pm: pseudo-pivoted {len(zeropivs)} times, smallest pivot used: {pivmin}'
+    info.number_of_times_ppivoted = len(zeropivs)
+    return pm, info
+
+
+def pm_info_to_string(info: PMInfo):
+    """
+    Returns a human readable rendering (String) of the PMInfo class.
+    :param info: Input PMInfo class
+    :return: Human readable string
+    """
+    s = f'Pseudo-pivoted {info.number_of_times_ppivoted} times, smallest pivot used: {info.smallest_pivot}'
+    if info.warn_not_odf:
+        s += ';  pm2mat: off diagonal zeros found, solution suspect.'
+    if info.warn_under_determined:
+        s += ';  pm2mat: multiple solutions to make rank(L-R)=1, solution suspect.'
+    if info.warn_inconsistent:
+        s += ';  pm2mat: input principal minors may be inconsistent, solution suspect.'
+    return s
 
 
 # Returns the numerical value of the most significant bit of x.
@@ -115,13 +144,10 @@ def pm2mat(pm: np.array):
     Run mat2pm on the output matrix a as needed.
     :param pm: The structure of pm, where |a[v]| is the principal minor of "a" indexed by the vector v is:
         pm: |a[1]|, |a[2]|, |a[1 2]|, |a[3]|, |a[1 3]|, |a[2 3]|, |a[1 2 3]|,...
-    :return: Tuple of (a: np.array, computation info: str)
+    :return: Tuple of (a: np.array, computation info: PMInfo)
     """
-    warn_not_odf = False
-    warn_under_determined = False
-    warn_inconsistent = False
 
-    def _invschurc(_pivot, _l, _r):
+    def _invschurc(_info: PMInfo, _pivot, _l, _r):
         """
         Suppose a is an (m+1) x (m+1) matrix such that
 
@@ -140,8 +166,6 @@ def pm2mat(pm: np.array):
         :param _r: _l - a[1:m+1, 0]*a[0, 1:m+1)/_pivot
         :return: Inverse Schur's complement
         """
-        nonlocal warn_under_determined
-        nonlocal warn_inconsistent
 
         # max allowable ratio of nearly zero row value to next largest row value
         myeps_i = 1e-3
@@ -152,7 +176,8 @@ def pm2mat(pm: np.array):
 
         # Try to make (L-R) rank 1
         if m == 2:
-            [t1, t2] = _solveright(_l[0, 0], _l[0, 1], _l[1, 0], _l[1, 1], _r[0, 0], _r[0, 1], _r[1, 0], _r[1, 1])
+            [t1, t2] = _solveright(_info, _l[0, 0], _l[0, 1], _l[1, 0], _l[1, 1], _r[0, 0], _r[0, 1], _r[1, 0],
+                                   _r[1, 1])
 
             # This is arbitrary, take the first.
             t = t1
@@ -167,11 +192,14 @@ def pm2mat(pm: np.array):
             i1 = m - 3
             i2 = m - 2
             i3 = m - 1
-            [r1, r2] = _solveright(_l[i2, i2], _l[i2, i3], _l[i3, i2], _l[i3, i3], _r[i2, i2], _r[i2, i3], _r[i3, i2],
+            [r1, r2] = _solveright(_info, _l[i2, i2], _l[i2, i3], _l[i3, i2], _l[i3, i3], _r[i2, i2], _r[i2, i3],
+                                   _r[i3, i2],
                                    _r[i3, i3])
-            [s1, s2] = _solveright(_l[i1, i1], _l[i1, i2], _l[i2, i1], _l[i2, i2], _r[i1, i1], _r[i1, i2], _r[i2, i1],
+            [s1, s2] = _solveright(_info, _l[i1, i1], _l[i1, i2], _l[i2, i1], _l[i2, i2], _r[i1, i1], _r[i1, i2],
+                                   _r[i2, i1],
                                    _r[i2, i2])
-            [t1, t2] = _solveright(_l[i1, i1], _l[i1, i3], _l[i3, i1], _l[i3, i3], _r[i1, i1], _r[i1, i3], _r[i3, i1],
+            [t1, t2] = _solveright(_info, _l[i1, i1], _l[i1, i3], _l[i3, i1], _l[i3, i3], _r[i1, i1], _r[i1, i3],
+                                   _r[i3, i1],
                                    _r[i3, i3])
             # Perform a parameterized "row reduction" on the first two rows of this
             # matrix and compute the absolute value of the (2,3) entry.  One of
@@ -212,10 +240,10 @@ def pm2mat(pm: np.array):
                 [r, s, t] = [r2, s2, t2]
 
             if mn > rv[1] * myeps_i:
-                warn_inconsistent = True
+                info.warn_inconsistent = True
 
             if np.count_nonzero(rv < myeps_c) > 1:
-                warn_under_determined = True
+                info.warn_under_determined = True
 
             _r[i3, i2] = _r[i3, i2] * r
             _r[i2, i3] = _r[i2, i3] / r
@@ -232,9 +260,9 @@ def pm2mat(pm: np.array):
 
                 # Now the inside lower right submatrix is done, so we
                 # only have 2 free parameters and 4 combinations to examine.
-                [s1, s2] = _solveright(_l[i1, i1], _l[i1, i2], _l[i2, i1], _l[i2, i2], _r[i1, i1], _r[i1, i2],
+                [s1, s2] = _solveright(_info, _l[i1, i1], _l[i1, i2], _l[i2, i1], _l[i2, i2], _r[i1, i1], _r[i1, i2],
                                        _r[i2, i1], _r[i2, i2])
-                [t1, t2] = _solveright(_l[i1, i1], _l[i1, i3], _l[i3, i1], _l[i3, i3], _r[i1, i1], _r[i1, i3],
+                [t1, t2] = _solveright(_info, _l[i1, i1], _l[i1, i3], _l[i3, i1], _l[i3, i3], _r[i1, i1], _r[i1, i3],
                                        _r[i3, i1], _r[i3, i3])
 
                 r11 = abs((_l[i2, i3] - _r[i2, i3]) * (_l[i1, i1] - _r[i1, i1]) -
@@ -257,10 +285,10 @@ def pm2mat(pm: np.array):
                     [s, t] = [s2, t2]
 
                 if mn > rv[1] * myeps_i:
-                    warn_inconsistent = True
+                    info.warn_inconsistent = True
 
                 if np.count_nonzero(rv < myeps_c) > 1:
-                    warn_under_determined = True
+                    info.warn_under_determined = True
 
                 _r[i2, i1] = _r[i2, i1] * s
                 _r[i1, i2] = _r[i1, i2] / s
@@ -277,7 +305,8 @@ def pm2mat(pm: np.array):
                     #
 
                     j1 = i1 + 1
-                    [t1, t2] = _solveright(_l[i1, i1], _l[i1, _j], _l[_j, i1], _l[_j, _j], _r[i1, i1], _r[i1, _j],
+                    [t1, t2] = _solveright(_info, _l[i1, i1], _l[i1, _j], _l[_j, i1], _l[_j, _j], _r[i1, i1],
+                                           _r[i1, _j],
                                            _r[_j, i1], _r[_j, _j])
                     r1 = abs((_l[j1, _j] - _r[j1, _j]) * (_l[i1, i1] - _r[i1, i1]) -
                              (_l[j1, i1] - _r[j1, i1]) * (_l[i1, _j] - _r[i1, _j] / t1))
@@ -291,10 +320,10 @@ def pm2mat(pm: np.array):
                     rv = sorted([r1, r2])
                     mn = rv[0]
                     if mn > rv[1] * myeps_i:
-                        warn_inconsistent = True
+                        info.warn_inconsistent = True
 
                     if np.count_nonzero(rv < myeps_c) > 1:
-                        warn_under_determined = True
+                        info.warn_under_determined = True
 
                     _r[_j, i1] = _r[_j, i1] * t
                     _r[i1, _j] = _r[i1, _j] / t
@@ -319,7 +348,7 @@ def pm2mat(pm: np.array):
         _a[1:m + 1, 1:m + 1] = _l
         return _a
 
-    def _solveright(_l11, _l12, _l21, _l22, _r11, _r12, _r21, _r22):
+    def _solveright(_info: PMInfo, _l11, _l12, _l21, _l22, _r11, _r12, _r21, _r22):
         """
         Returns the two possible real solutions that will make L-R rank one if we let
         r21 = r21*t? (where ? = 1 or 2) and
@@ -335,11 +364,11 @@ def pm2mat(pm: np.array):
         :param _r22:
         :return: Tuple of real solutions
         """
-        nonlocal warn_not_odf
         _x1 = _l11 - _r11
         _x2 = _l22 - _r22
-        _d = np.sqrt(_x1*_x1*_x2*_x2 + _l12*_l12*_l21*_l21 + _r12*_r12*_r21*_r21 - 2*_x1*_x2*_l12*_l21 -
-                     2*_x1*_x2*_r12*_r21 - 2*_l12*_l21*_r21*_r12)
+        _d = np.sqrt(
+            _x1 * _x1 * _x2 * _x2 + _l12 * _l12 * _l21 * _l21 + _r12 * _r12 * _r21 * _r21 - 2 * _x1 * _x2 * _l12 * _l21
+            - 2 * _x1 * _x2 * _r12 * _r21 - 2 * _l12 * _l21 * _r21 * _r12)
         if (_l12 == 0) or (_r21 == 0):
             # This shouldn't happen normally, but to prevent
             # divide by zero when we set all "dependent" principal
@@ -347,14 +376,14 @@ def pm2mat(pm: np.array):
             # to the same value, let [t1,t2] be something.
             _t1 = 1
             _t2 = 1
-            warn_not_odf = True
+            info.warn_not_odf = True
         else:
-            _t1 = (-_x1*_x2 + _l12*_l21 + _r12*_r21 - _d) / (2*_l12*_r21)
-            _t2 = (-_x1*_x2 + _l12*_l21 + _r12*_r21 + _d) / (2*_l12*_r21)
+            _t1 = (-_x1 * _x2 + _l12 * _l21 + _r12 * _r21 - _d) / (2 * _l12 * _r21)
+            _t2 = (-_x1 * _x2 + _l12 * _l21 + _r12 * _r21 + _d) / (2 * _l12 * _r21)
 
             # This also shouldn't happen.  Comment above applies.
             if _t1 == 0 or _t2 == 0:
-                warn_not_odf = True
+                info.warn_not_odf = True
                 if _t1 == 0 and _t2 == 0:
                     _t1 = 1
                     _t2 = 1
@@ -368,7 +397,7 @@ def pm2mat(pm: np.array):
 
     myeps = 1e-10
     pm = pm.astype(DTYPE)
-    n = int(round(np.log2(len(pm)+1)))
+    n = int(round(np.log2(len(pm) + 1)))
 
     # Make first (smallest) entry of zeropivs an impossible index
     zeropivs = [-1]
@@ -379,7 +408,7 @@ def pm2mat(pm: np.array):
     # To avoid division by zero, do an operation analogous to the zeropivot
     # loop in mat2pm.
     _pm = np.copy(pm)
-    for i in range((len(_pm)+1)//2 - 1):
+    for i in range((len(_pm) + 1) // 2 - 1):
         if np.abs(_pm[i]) < myeps:
             mask = i + 1
             zeropivs.append(i)
@@ -387,27 +416,27 @@ def pm2mat(pm: np.array):
             if ipm1 == 0:
                 _pm[i] += ppivot
             else:
-                _pm[i] = (_pm[i]/_pm[ipm1-1] + ppivot)*_pm[ipm1-1]
+                _pm[i] = (_pm[i] / _pm[ipm1 - 1] + ppivot) * _pm[ipm1 - 1]
             delta = _msb(mask)
-            delta2 = 2*delta
-            for j in range(mask+delta2-1, len(_pm), delta2):
-                _pm[j] += ppivot*_pm[j - delta]
+            delta2 = 2 * delta
+            for j in range(mask + delta2 - 1, len(_pm), delta2):
+                _pm[j] += ppivot * _pm[j - delta]
     zeropivsidx = len(zeropivs) - 1
     zeropivsmax = zeropivs[zeropivsidx]
     zeropivsidx -= 1
 
     # initial processing is special, no call to invschurc is necessary
-    nq = (len(_pm)+1) // 2
+    nq = (len(_pm) + 1) // 2
     n1 = 1
-    q = [None] * nq   # zeros(1,1,nq)
-    ipm1 = nq-1
+    q = [None] * nq  # zeros(1,1,nq)
+    ipm1 = nq - 1
     ipm2 = 0
     for i in range(nq):
         if i == 0:
             pivot = _pm[ipm1]
             q[i] = np.array([[pivot]], dtype=DTYPE)
         else:
-            pivot = _pm[ipm1]/_pm[ipm2]
+            pivot = _pm[ipm1] / _pm[ipm2]
             q[i] = np.array([[pivot]], dtype=DTYPE)
             ipm2 += 1
         ipm1 += 1
@@ -415,24 +444,25 @@ def pm2mat(pm: np.array):
     #
     # Main 'level' loop
     #
-    pivmin = np.finfo(np.float64).max
-    for level in range(n-2, -1, -1):  # just counts? = n-2:-1:0        # for consistency with mat2pm levels
+    info = PMInfo()
+
+    for level in range(n - 2, -1, -1):  # just counts? = n-2:-1:0        # for consistency with mat2pm levels
         nq = len(q) // 2
-        n1 = n1+1
+        n1 = n1 + 1
         # The output queue has half the number of matrices, each one larger in
         # row and col dimension
-        qq = [None] * nq        # zeros(n1, n1, nq);
+        qq = [None] * nq  # zeros(n1, n1, nq);
 
-        ipm1 = 2*nq-2
-        ipm2 = nq-2
-        for i in range(nq-1, -1, -1):  # = nq:-1:1     # process matrices in reverse order for zeropivs
+        ipm1 = 2 * nq - 2
+        ipm2 = nq - 2
+        for i in range(nq - 1, -1, -1):  # = nq:-1:1     # process matrices in reverse order for zeropivs
             if i == 0:
                 pivot = _pm[ipm1]
             else:
-                pivot = _pm[ipm1]/_pm[ipm2]
+                pivot = _pm[ipm1] / _pm[ipm2]
                 ipm2 -= 1
-            pivmin = min(pivmin, np.abs(pivot))
-            qq[i] = _invschurc(pivot, q[i], q[i+nq])
+            info.smallest_pivot = min(info.smallest_pivot, np.abs(pivot))
+            qq[i] = _invschurc(info, pivot, q[i], q[i + nq])
             if zeropivsmax == ipm1:
                 qq[i][0][0] -= ppivot
                 zeropivsmax = zeropivs[zeropivsidx]
@@ -441,14 +471,7 @@ def pm2mat(pm: np.array):
         q = qq
     a = q[0]
     _deskew(a)
-    warn = f'pm2mat: pseudo-pivoted {len(zeropivs)-1} times, smallest pivot used: {pivmin}'
-    if warn_not_odf:
-        warn = (warn + ';  ' if warn else '') + 'pm2mat: off diagonal zeros found, solution suspect.'
-    if warn_under_determined:
-        warn = (warn + ';  ' if warn else '') + 'pm2mat: multiple solutions to make rank(L-R)=1, solution suspect.'
-    if warn_inconsistent:
-        warn = (warn + ';  ' if warn else '') + 'pm2mat: input principal minors may be inconsistent, solution suspect.'
-    return a, warn
+    return a, info
 
 
 #
@@ -460,12 +483,12 @@ def _deskew(_a: np.array):
     :param _a: Square matrix
     :return: Deskewed square matrix (but also modifies _a by reference)
     """
-    assert(len(_a.shape) == 2 and _a.shape[0] == _a.shape[1])
+    assert (len(_a.shape) == 2 and _a.shape[0] == _a.shape[1])
     _n = _a.shape[0]
     _d = np.ones(_n)
     for _i in range(1, _n):
         if _a[_i, 0] != 0:  # don't divide by 0
-            _d[_i] = np.sqrt(np.abs(_a[0, _i]/_a[_i, 0]))
+            _d[_i] = np.sqrt(np.abs(_a[0, _i] / _a[_i, 0]))
             if _d[_i] > 1e6 or _d[_i] < 1e-6:
                 # something is likely wrong, use 1 instead
                 _d[_i] = 1.
@@ -473,7 +496,7 @@ def _deskew(_a: np.array):
 
     # If D = diag(d), this effectively computes A = D*A*inv(D)
     for _i in range(1, _n):
-        _a[_i, :] = _a[_i, :]*_d[_i]
+        _a[_i, :] = _a[_i, :] * _d[_i]
     for _i in range(1, _n):
-        _a[:, _i] = _a[:, _i]/_d[_i]
+        _a[:, _i] = _a[:, _i] / _d[_i]
     return _a
--- Python/test/test_mat2pm.py
+++ Python/test/test_mat2pm.py
@@ -1,9 +1,11 @@
-import numpy as np
-from nose.tools import assert_equal, assert_in
-from numpy.testing import assert_array_equal, assert_array_almost_equal
-from principal_minor_calculations import mat2pm, pm2mat, _msb
 from cProfile import Profile
 from pstats import Stats
+
+import numpy as np
+from nose.tools import assert_equal
+from numpy.testing import assert_array_equal, assert_array_almost_equal, assert_almost_equal
+
+from principal_minor_calculations import mat2pm, pm2mat, _msb, PMInfo, pm_info_to_string
 
 
 def test_msb():
@@ -16,59 +18,77 @@ def test_msb():
     assert_equal(_msb(0xf234567812345678), 0x8000000000000000)
 
 
-def test_mat2pm():
+def test_pm_calcs():
     m1 = np.array([[1, 2],
                    [3, 4]])
-    pm1, msg = mat2pm(m1)
+    pm1, info = mat2pm(m1)
     assert_array_equal(pm1, [1, 4, -2])
-    m2, msg = pm2mat(pm1)
-    pm2, msg = mat2pm(m2)
+    m2, info = pm2mat(pm1)
+    pm2, info = mat2pm(m2)
     assert_array_equal(pm1, pm2)
 
     m1 = np.array([[1, 2, 6],
                    [2, 4, 5],
                    [-1, 2, 3]])
-    pm1, msg = mat2pm(m1)
+    pm1, info = mat2pm(m1)
     assert_array_equal(pm1, [1, 4, 0, 3, 9, 2, 28])
-    m2, msg = pm2mat(pm1)
-    pm2, msg = mat2pm(m2)
+    m2, info = pm2mat(pm1)
+    pm2, info = mat2pm(m2)
     assert_array_almost_equal(pm1, pm2, decimal=10)
 
     m1 = np.array([[0, 1, 0, 0],
                    [0, 0, 1, 0],
                    [0, 0, 0, 1],
                    [1, 0, 0, 0]])
-    pm1, msg = mat2pm(m1)
+    pm1, info = mat2pm(m1)
     assert_array_equal(pm1, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1])
-    assert_equal(msg, 'mat2pm: pseudo-pivoted 7 times, smallest pivot used: 0.25')
-    m2, msg = pm2mat(pm1)
+    assert_equal(info.number_of_times_ppivoted, 7)
+    assert_equal(info.smallest_pivot, 0.25)
+    m2, info = pm2mat(pm1)
+    assert_equal(info.number_of_times_ppivoted, 0)
+    assert_almost_equal(info.smallest_pivot, 1.9501292851471754, decimal=10)
     assert_array_equal(m2, [[0, 1, 1, 1],
                             [0, 0, 1, 1],
                             [0, 0, 0, 1],
                             [1, 0, 0, 0]])
-    assert_in('off diagonal zeros found', msg)
-    assert_in('multiple solutions to make rank', msg)
+    assert info.warn_not_odf
+    assert info.warn_under_determined
+    assert not info.warn_inconsistent
+
     pm_inconsistent = np.array([-6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8])
-    m, msg = pm2mat(pm_inconsistent)
-    assert_in('principal minors may be inconsistent', msg)
-    pm, msg = mat2pm(m)
+    m, info = pm2mat(pm_inconsistent)
+    assert not info.warn_not_odf
+    assert not info.warn_under_determined
+    assert info.warn_inconsistent
+    pm, info = mat2pm(m)
     # all but two of the principal minors actually match
-    pm[12] = 0
-    pm[14] = 0
-    pm_inconsistent[12] = 0
-    pm_inconsistent[14] = 0
+    pm[12] = 6.
+    pm[14] = 8.
     assert_array_almost_equal(pm, pm_inconsistent, decimal=10)
 
     m1 = np.array([[-6, 3, -9, 4],
                    [-6, -5, 3, 6],
                    [3, -3, 6, -7],
                    [1, 1, -1, -3]])
-    pm1, msg = mat2pm(m1)
+    pm1, info = mat2pm(m1)
     pm_truth = np.array([-6, -5, 48, 6, -9, -21, -36, -3, 14, 9, -94, -25, 96, 59, 6])
-    assert_equal(msg, 'mat2pm: pseudo-pivoted 0 times, smallest pivot used: 0.75')
+    assert_equal(info.number_of_times_ppivoted, 0)
+    assert_equal(info.smallest_pivot, 0.75)
     assert_array_almost_equal(pm1, pm_truth, decimal=10)
-    m2, msg = pm2mat(pm_truth)
-    pm2, msg = mat2pm(m2)
+    m2, info = pm2mat(pm_truth)
+    pm2, info = mat2pm(m2)
+    assert_array_almost_equal(pm1, pm2, decimal=10)
+
+    m1 = np.array([[1 + 1j,  3 - 4j, -2 - 1j],
+                   [7 + 3j, -5 + 2j, -1 - 1j],
+                   [0 - 4j,  3 + 0j,  3 + 3j]])
+    pm1, info = mat2pm(m1)
+    pm_truth = np.array([1.0 + 1.0j, -5.0 + 2.0j, -40.0 + 16.0j, 3.0 + 3.0j, 4.0 - 2.0j, -18.0 - 6.0j, -201.0 - 29.0j])
+    assert_equal(info.number_of_times_ppivoted, 0)
+    assert_almost_equal(info.smallest_pivot, np.sqrt(2))
+    assert_array_almost_equal(pm1, pm_truth, decimal=10)
+    m2, info = pm2mat(pm_truth)
+    pm2, info = mat2pm(m2)
     assert_array_almost_equal(pm1, pm2, decimal=10)
 
     n = 18
@@ -78,8 +98,8 @@ def test_mat2pm():
     if do_profile:
         profiler = Profile()
         profiler.enable()
-    pm1, msg = mat2pm(m1)
-    m2, msg = pm2mat(pm1)
+    pm1, info = mat2pm(m1)
+    m2, info = pm2mat(pm1)
     if do_profile:
         profiler.disable()
         stats = Stats(profiler)
@@ -87,7 +107,24 @@ def test_mat2pm():
         stats.sort_stats('cumulative')
         stats.print_stats()
 
-    pm2, msg = mat2pm(m2)
+    pm2, info = mat2pm(m2)
     assert_equal(len(pm1), 2 ** n - 1)
     assert_equal(len(pm2), 2 ** n - 1)
-    assert_array_almost_equal(pm1, pm2, decimal=8)
+    assert_array_almost_equal(pm1, pm2, decimal=7)
+
+
+def test_pm_info_to_string():
+    info = PMInfo()
+    info.smallest_pivot = 3.14
+    info.number_of_times_ppivoted = 42
+    s = pm_info_to_string(info)
+    assert_equal(s, 'Pseudo-pivoted 42 times, smallest pivot used: 3.14')
+    info.warn_not_odf = True
+    info.warn_under_determined = True
+    info.warn_inconsistent = True
+    s = pm_info_to_string(info)
+    assert_equal(s, '\
+Pseudo-pivoted 42 times, smallest pivot used: 3.14;  \
+pm2mat: off diagonal zeros found, solution suspect.;  \
+pm2mat: multiple solutions to make rank(L-R)=1, solution suspect.;  \
+pm2mat: input principal minors may be inconsistent, solution suspect.')
